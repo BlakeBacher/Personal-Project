45cc858c595624271acef700b49e7b11b68b78af
Blake Rindlisbacher <blakebacher@gmail.com>
2018-07-28 12:20:38 -0600

    added alert for desktop view

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,14 +3,23 @@ import './Login.css'
 
 export default class Login extends Component {
     render() {
-        return (
-            <div className = 'login'>
-                <div className = 'logindiv'>
-                    <div className='h1'>KjoStyles</div> 
-                    <a href = {process.env.REACT_APP_LOGIN}>
-                        <button className='button'>Login</button>
-                    </a> 
-                </div> 
+            if(window.innerWidth < 500){
+                return(
+                    <div className = 'login'>
+                        <div className = 'logindiv'>
+                            <div className='h1'>KjoStyles</div> 
+                            <a href = {process.env.REACT_APP_LOGIN}>
+                                <button className='button'>Login</button>
+                            </a> 
+                        </div> 
+                    </div>
+                )
+            }
+        return(
+            <div style ={{ height: '100vh', display: 'flex',flexDirection:'column', justifyContent: 'center', alignItems: 'center', fontSize: '30px'}}>
+                <p>Please switch to a mobile device. Thank you.</p>
+                <br/>
+                <p>- kjostyles</p>
             </div> 
         )
     }
